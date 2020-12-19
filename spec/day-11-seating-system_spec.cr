ad271fe04c91153ef42c35d2e25af8eeb249beec
Mike Jongen <mikejongen@hotmail.com>
2020-12-19 16:55:38 +0100

    initial commit

---
+++ spec/day-11-seating-system_spec.cr
@@ -0,0 +1,9 @@
+require "./spec_helper"
+
+describe Day::11::Seating::System do
+  # TODO: Write tests
+
+  it "works" do
+    false.should eq(true)
+  end
+end
---
+++ src/day-11-seating-system.cr
@@ -0,0 +1,6 @@
+# TODO: Write documentation for `Day::11::Seating::System`
+module Day::11::Seating::System
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+end
